0265c66bf7a48ec66c19d891a13f5e00d432492f
Lumic-code <wisdomlumic0368@gmail.com>
2023-10-15 22:20:21 +0100

    first part of the project done

---
+++ Dapper.Infrastructure/Repository/ProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using Dapper.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dapper.Infrastructure.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+
+    }
+}
